4090b29b7a57c90d4cc377e7958db0d5a949ec2f
adilm71 <146095499+adilm71@users.noreply.github.com>
2023-11-17 14:33:14 +0000

    part 1 nearly done

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,32 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { PagesList } from './PagesList';
+import React, { useState, useEffect } from "react";
+import { PagesList } from "./PagesList";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
 
 export const App = () => {
+  const [pages, setPages] = useState([]);
+  const [article, setArticle] = useState("");
 
-	const [pages, setPages] = useState([]);
+  async function fetchPages() {
+    try {
+      const response = await fetch(`${apiURL}/wiki`);
+      const pagesData = await response.json();
+      setPages(pagesData);
+    } catch (err) {
+      console.log("Oh no an error! ", err);
+    }
+  }
 
-	async function fetchPages(){
-		try {
-			const response = await fetch(`${apiURL}/wiki`);
-			const pagesData = await response.json();
-			setPages(pagesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
+  useEffect(() => {
+    fetchPages();
+  }, []);
 
-	useEffect(() => {
-		fetchPages();
-	}, []);
+  const fetchArticle = async (slug) => {
+    const response = await fetch(`${apiURL}/wiki/${slug}`);
+    const data = await response.json();
+    setArticle(data.content);
+  };
 
-	return (
-		<main>	
+  return (
+    <main>
       <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
-		</main>
-	)
-}
+      <h2>An interesting 📚</h2>
+      {article === "" && (
+        <PagesList pages={pages} fetchArticle={fetchArticle} />
+      )}{" "}
+      {article != "" && (
+        <>
+          <p>{article}</p>
+          <button onClick={() => setArticle("")}>Back to WikiVerse</button>
+        </>
+      )}
+    </main>
+  );
+};
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export const Page = (props) => {
-
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-} 
-	
+  return (
+    <>
+      <h3>{props.page.title}</h3>
+      <button onClick={() => props.fetchArticle(props.page.slug)}>
+        Read this article
+      </button>
+    </>
+  );
+};
